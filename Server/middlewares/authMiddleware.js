14b73fb2e3a4290957f1a2be2296e27734ed83d3
ashutos-h-tiwari <tiwariashutosh3884@gmail.com>
2026-02-26 04:01:25 +0530

    deployment issue

--- Server/middlewares/authMiddleware.js
+++ Server/middlewares/authMiddleware.js
@@ -3,7 +3,7 @@ import User from "../models/User.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const protect = auth; // alias
+
 export const auth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader?.startsWith("Bearer ")) return res.status(401).json({ message: "Unauthorized" });
@@ -19,3 +19,4 @@ export const auth = async (req, res, next) => {
     return res.status(401).json({ message: "Invalid token" });
   }
 };
+export const protect = auth; // alias
