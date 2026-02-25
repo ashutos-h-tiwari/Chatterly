dd4d81f136627a6465db4b4f25aff604f159e1ef
ashutos-h-tiwari <tiwariashutosh3884@gmail.com>
2026-02-26 03:50:12 +0530

    Fix authMiddleware import (named export)

--- Server/routes/notificationRoutes.js
+++ Server/routes/notificationRoutes.js
@@ -1,10 +1,9 @@
 import express from "express";
 import { saveFcmToken } from "../controllers/notificationController.js";
-import auth from "../middlewares/authMiddleware.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 // Save / update FCM token
-router.post("/fcm-token", auth, saveFcmToken);
-
+router.post("/fcm-token", protect, saveFcmToken);
 export default router;
