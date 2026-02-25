be94030ab3d15b24f2a9254d9c2556d69735e35b
ashutos-h-tiwari <tiwariashutosh3884@gmail.com>
2026-02-26 03:44:48 +0530

    resolving deployment issues

--- Server/routes/notificationRoutes.js
+++ Server/routes/notificationRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { saveFcmToken } from "../controllers/notificationController.js";
-import { protect } from "../middlewares/authMiddleware.js";
+import auth from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 // Save / update FCM token
-router.post("/fcm-token", protect, saveFcmToken);
+router.post("/fcm-token", auth, saveFcmToken);
 
 export default router;
