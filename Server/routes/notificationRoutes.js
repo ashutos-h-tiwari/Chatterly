ec92d3ad7e516522eb6842d1e69927c09fb0f96f
ashutos-h-tiwari <tiwariashutosh3884@gmail.com>
2026-02-26 04:04:00 +0530

    dI

--- Server/routes/notificationRoutes.js
+++ Server/routes/notificationRoutes.js
@@ -2,9 +2,9 @@ import express from "express";
 import { saveFcmToken } from "../controllers/notificationController.js";
 import { auth } from "../middlewares/authMiddleware.js";
 
-router.post("/fcm-token", auth, saveFcmToken);
 
 const router = express.Router();
+router.post("/fcm-token", auth, saveFcmToken);
 
 // Save / update FCM token
 //router.post("/fcm-token", protect, saveFcmToken);
